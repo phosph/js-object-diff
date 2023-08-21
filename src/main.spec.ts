93d98dda8e0338daee90f521d936561a58ac6661
Jesus Figueredo <jesus@figueredo.dev>
2023-08-21 00:18:04 -0300

    feat: flat diff representation for ArrayDiff and ObjectDiff; and small improvements

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,95 +1,227 @@
-import { Diff, NoDiff, ValueDiff, StringDiff, ObjectDiff, ArrayDiff } from './main';
-
-// TODO: tests
+import { ArrayDiff, Diff, JsonPath, NoDiff, ObjectDiff, StringDiff, ValueDiff, compare } from './main';
 
 describe('object diff test', () => {
-  it('Diff.compare(null,null)', () => {
+  it('compare(null,null)', () => {
     const a = null
     const b = null
-    const result = Diff.compare(a,b)
+    const result = compare(a, b)
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(NoDiff)
   })
-  it('Diff.compare(1,1)', () => {
+  it('compare(1,1)', () => {
     const a = 1
     const b = 1
-    const result = Diff.compare(a,b)
+    const result = compare(a, b)
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(NoDiff)
   })
-  it('Diff.compare("", "")', () => {
+  it('compare("", "")', () => {
     const a = ''
     const b = ''
-    const result = Diff.compare(a,b)
+    const result = compare(a, b)
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(NoDiff)
   })
-  it('Diff.compare(undefined,undefined)', () => {
+  it('compare(undefined,undefined)', () => {
     const a = undefined
     const b = undefined
-    const result = Diff.compare(a,b)
+    const result = compare(a, b)
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(NoDiff)
   })
-  it('Diff.compare(Number,Number)', () => {
+  it('compare(Number,Number)', () => {
     const a = Math.random()
     const b = Math.random()
-    const result = Diff.compare(a,b)
+    const result = compare(a, b)
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(ValueDiff)
     expect(result.diff).toBe(true)
     expect((result as ValueDiff<any, any>).__old).toBe(a)
     expect((result as ValueDiff<any, any>).__new).toBe(b)
   })
-  it('Diff.compare("a","b")', () => {
+  it('compare("a","b")', () => {
     const a = 'a'
     const b = 'b'
-    const result = Diff.compare(a,b)
+    const result = compare(a, b)
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(StringDiff)
     expect(result.diff).toBe(true)
-    expect((result as StringDiff).__old).toBe(a)
-    expect((result as StringDiff).__new).toBe(b)
-    expect((result as StringDiff).strDiff).toBeInstanceOf(Array) // TODO, better thes
+    expect(result.__old).toBe(a)
+    expect(result.__new).toBe(b)
+    expect(result.strDiff).toBeInstanceOf(Array) // TODO, better thes
   })
-  it('Diff.compare({},{})', () => {
+  it('compare({},{})', () => {
     const a = {}
     const b = {}
-    const result = Diff.compare(a, b);
+    const result = compare(a, b);
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(ObjectDiff)
     expect(result.diff).not.toBeTruthy()
 
 
-    expect((result as ObjectDiff<{},{}>).nested).toBeTruthy()
-    expect((result as ObjectDiff<{},{}>).internal).toBeInstanceOf(Object);
-    expect((result as ObjectDiff<{},{}>).internal).not.toBeNull()
-    expect((result as ObjectDiff<{},{}>).internal).not.toBeInstanceOf(Array);
-    for (let val of Object.values((result as ObjectDiff<{},{}>).internal)) {
+    expect(result.nested).toBeTruthy()
+    expect(result.internal).toBeInstanceOf(Object);
+    expect(result.internal).not.toBeNull()
+    expect(result.internal).not.toBeInstanceOf(Array);
+    for (let val of Object.values(result.internal)) {
       expect(val).toBeInstanceOf(Diff);
     }
   })
-  it('Diff.compare([],[])', () => {
+  it('compare([],[])', () => {
     const a = [] as any[]
     const b = [] as any[]
-    const result = Diff.compare(a,b)
+    const result = compare(a, b)
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(ArrayDiff)
     expect(result.diff).not.toBeTruthy()
-    expect((result as ArrayDiff).nested).toBeTruthy()
-    expect((result as ArrayDiff).internal).toBeInstanceOf(Array)
-    for(let val of (result as ArrayDiff).internal) {
+    expect(result.nested).toBeTruthy()
+    expect(result.internal).toBeInstanceOf(Array)
+    for (let val of result.internal) {
       expect(val).toBeInstanceOf(Diff)
     }
   })
-  it('Diff.compare([],{})', () => {
+  it('compare([],{})', () => {
     const a: any[] = []
     const b = {}
-    const result = Diff.compare(a,b)
+    const result = compare(a, b)
     expect(result).toBeInstanceOf(Diff)
     expect(result).toBeInstanceOf(ValueDiff)
     expect(result.diff).toBeTruthy()
     expect((result as ValueDiff<any, any>).__old).toBe(a)
     expect((result as ValueDiff<any, any>).__new).toBe(b)
+  })
+})
+
+describe('radom Comparition', () => {
+  const ramdon = ['afaerg', Math.random() * (Math.random() * 4 | 0), null, undefined, {}, [], 'ñlaieurgnv', Math.random()]
+
+  for (const obj1 of ramdon) {
+    const obj2 = ramdon[Math.random() * 23 % ramdon.length];
+
+    const str1 = typeof obj1 === 'string' ? `"${obj1}"`
+      : Array.isArray(obj1) ? '[]'
+        : (typeof obj1 === 'object' && obj1 !== null) ? '{}'
+          : obj1
+    const str2 = typeof obj2 === 'string' ? `"${obj2}"`
+      : Array.isArray(obj2) ? '[]'
+        : (typeof obj2 === 'object' && obj2 !== null) ? '{}'
+          : obj2
+
+    it(`compare(${str1}, ${str2})`, () => {
+        const diff = compare(obj1, obj2);
+        if (obj1 === obj2) {
+          expect(diff).toBeInstanceOf(NoDiff);
+        } else if (typeof obj1 !== typeof obj2) {
+          expect(diff).toBeInstanceOf(ValueDiff);
+        } else if (Array.isArray(obj1)) {
+          expect(diff).toBeInstanceOf(ArrayDiff);
+        } else if (obj1 === null) {
+          expect(diff).toBeInstanceOf(ValueDiff);
+        } else if (typeof obj1 === 'object') {
+          expect(diff).toBeInstanceOf(ObjectDiff);
+        }
+      })
+  }
+
+})
+
+
+describe('ObjectDiff.flat', () => {
+  const obj1 = { a: 'hello', c: 'hello', d: [{ word: 'hello' }] }
+  const obj2 = { b: 'world', c: 'world', d: [{ word: 'world' }] }
+  const diff = compare(obj1, obj2);
+
+  it('instanceof ObjectDiff', () => {
+    expect(diff).toBeInstanceOf(ObjectDiff);
+    expect(diff.diff).toBeTruthy();
+  });
+
+  it('flat', () => {
+    console.time('ObjectDiff.flat contruction')
+    const flat = diff.flat;
+    console.timeEnd('ObjectDiff.flat contruction')
+    expect(flat).toBeInstanceOf(Array)
+    expect(flat).toHaveLength(4)
+    for (const item of flat) {
+      expect(item).toBeInstanceOf(Array)
+      expect(item).toHaveLength(2)
+      expect(item[0]).toBeInstanceOf(JsonPath)
+      expect(item[1]).toBeInstanceOf(Diff)
+    }
+  })
+
+  it('flat[0]', () => {
+    const [path, _diff] = diff.flat[0]
+    expect(path.toString()).toBe('$.a')
+    expect(_diff).toBeInstanceOf(ValueDiff)
+  })
+
+  it('flat[1]', () => {
+    const [path, _diff] = diff.flat[1]
+    expect(path.toString()).toBe('$.b')
+    expect(_diff).toBeInstanceOf(ValueDiff)
+  })
+
+  it('flat[2]', () => {
+    const [path, _diff] = diff.flat[2]
+    expect(path.toString()).toBe('$.c')
+    expect(_diff).toBeInstanceOf(StringDiff)
+  })
+  it('flat[3]', () => {
+    const [path, _diff] = diff.flat[3]
+    expect(path.toString()).toBe('$.d[0].word')
+    expect(_diff).toBeInstanceOf(StringDiff)
+  })
+})
+describe('ArrayDiff.flat', () => {
+  const obj1 = ['hello', [{ word: 'hello' }]];
+  const obj2 = ['world', [{ word: 'world' }]];
+  const diff = compare(obj1, obj2);
+
+  it('instanceof ArrayDiff', () => {
+    expect(diff).toBeInstanceOf(ArrayDiff);
+    expect(diff.diff).toBeTruthy();
+  });
+
+  it('flat', () => {
+    console.time('ArrayDiff.flat contruction')
+    const flat = diff.flat;
+    console.timeEnd('ArrayDiff.flat contruction')
+    expect(flat).toBeInstanceOf(Array)
+    expect(flat).toHaveLength(2)
+    for (const item of flat) {
+      expect(item).toBeInstanceOf(Array)
+      expect(item).toHaveLength(2)
+      expect(item[0]).toBeInstanceOf(JsonPath)
+      expect(item[1]).toBeInstanceOf(Diff)
+    }
+  })
+
+  it('flat[0]', () => {
+    const [path, _diff] = diff.flat[0]
+    expect(path.toString()).toBe('$[0]')
+    expect(_diff).toBeInstanceOf(StringDiff)
+  })
+
+  it('flat[1]', () => {
+    const [path, _diff] = diff.flat[1]
+    expect(path.toString()).toBe('$[1][0].word')
+    expect(_diff).toBeInstanceOf(StringDiff)
+  })
+})
+
+describe('JsonPath', () => {
+  const path = [1, 0, 'word']
+  const instance = JsonPath.from(path);
+  it('JsonPath', () => {
+    expect(instance).toBeInstanceOf(JsonPath);
+    expect(instance).toHaveLength(3);
+    expect(instance).toMatchObject(path);
+  })
+
+  it('Symbol.toPrimitive', () => {
+    expect(instance.toString()).toBe(`$[${path[0]}][${path[1]}].${path[2]}`);
+    expect(`${instance}`).toBe(`$[${path[0]}][${path[1]}].${path[2]}`);
+    expect(+instance).toBe(instance.length);
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { diffChars, Change as DiffChange } from 'diff';
+import { Change as DiffChange, diffChars } from 'diff';
 
 interface INoDiff {
   readonly diff: false;
@@ -23,36 +23,79 @@ interface IArrayDiff {
   readonly internal: Diff<any, any>[];
 }
 
-export abstract class Diff<T = any,U = any> {
+export class JsonPath extends Array<string | number> {
+  #jsonPathCache: string | null = null;
+  toJsonPath(): string {
+    if (this.#jsonPathCache !== null) return this.#jsonPathCache;
+
+    let path = '$';
+    for (const key of this) {
+      if (typeof key === 'number') path += `[${key}]`;
+      else path += `.${key}`;
+    }
+
+    this.#jsonPathCache = path;
+    return path;
+  }
+
+  toString(): string {
+    return this.toJsonPath();
+  }
+
+  [Symbol.toPrimitive](hint: "number" | "string" | "default") {
+    if (hint === 'number') return this.length;
+    return this.toString();
+  }
+
+  static from(iterable: Iterable<string | number> | ArrayLike<string | number>) {
+    const instance = new JsonPath()
+
+    if (Symbol.iterator in iterable) for (const item of iterable) instance.push(item)
+    else for (let i = 0; i < iterable.length; i++) instance.push(iterable[i])
+
+    return instance;
+  }
+
+}
+
+export abstract class Diff<T = any, U = any> {
   constructor(
     public readonly obj1: T,
     public readonly obj2: U
   ) { }
 
-  static compare<R,S>(obj1:R, obj2: S) {
-    return diff<R,S>(obj1, obj2);
+  static compare<R, S>(obj1: R, obj2: S) {
+    return compare<R, S>(obj1, obj2);
   }
   abstract readonly diff: any
 }
-export class NoDiff<T> extends Diff<T,T> implements INoDiff {
+
+export class NoDiff<T> extends Diff<T, T> implements INoDiff {
   readonly diff = false;
 
   constructor(obj1: T, obj2: T) {
     if (!Object.is(obj1, obj2)) throw new TypeError('are not the same')
-    super(obj1, obj2)
+    super(obj1, obj2);
   }
 
   get [Symbol.toStringTag]() { return 'NoDiff'; }
 
   get value() { return this.obj1 }
 }
-export class ValueDiff<T,U> extends Diff<T,U> implements IValueDiff {
+
+export class ValueDiff<T, U> extends Diff<T, U> implements IValueDiff {
   get [Symbol.toStringTag]() { return 'ValueDiff'; }
+
+  constructor(obj1: T, obj2: U) {
+    if (Object.is(obj1, obj2)) throw new TypeError('are he same')
+    super(obj1, obj2);
+  }
 
   readonly diff = true;
   get __old() { return this.obj1 }
   get __new() { return this.obj2 }
 }
+
 export class StringDiff extends ValueDiff<string, string> implements IStringDiff {
   get [Symbol.toStringTag]() {
     return 'StringDiff';
@@ -65,90 +108,162 @@ export class StringDiff extends ValueDiff<string, string> implements IStringDiff
     return this._cachedDiff!;
   }
 }
+
 export class ObjectDiff<T extends Record<string, any>, U extends Record<string, any>> extends Diff<T, U> implements INestedDiff<T, U> {
 
   get [Symbol.toStringTag]() { return 'NestedDiff'; }
 
   readonly nested = true;
 
-  protected _cachedDiffValue: INestedDiff<T,U>['diff'] | null = null;
+  protected _cachedDiffValue: INestedDiff<T, U>['diff'] | null = null;
   get diff() {
     if (this._cachedDiffValue === null) this.calcDeps()
     return this._cachedDiffValue!;
   };
 
-  protected _cachedInternal: INestedDiff<T,U>['internal'] | null = null;
+  protected _cachedInternal: INestedDiff<T, U>['internal'] | null = null;
   get internal() {
     if (this._cachedInternal === null) this.calcDeps()
     return this._cachedInternal!;
   }
 
   protected calcDeps() {
-    const a = Object.keys(this.obj1)
-      .concat(Object.keys(this.obj2))
-      .filter((item, index, self) => self.indexOf(item) === index)
-      .map(key => [key, diff(this.obj1[key], this.obj2[key])]) as [string, Diff][]
+    const keys = new Set([
+      ...Object.keys(this.obj1),
+      ...Object.keys(this.obj2),
+    ]);
 
-    this._cachedDiffValue = !!a.find(([_, value]) => value.diff)
+    const diffs: [keys: string, diff: Diff][] = []
 
-    this._cachedInternal = Object.fromEntries(a);
+    for (let key of keys) {
+      const _diff = compare(this.obj1[key], this.obj2[key]);
+      diffs.push([key, _diff]);
+
+      if (_diff.diff) this._cachedDiffValue ||= true
+    }
+
+    this._cachedInternal = Object.freeze(Object.fromEntries(diffs));
   }
+
+  #flatCache: [JsonPath, Diff][] | null = null
+  get flat() {
+    this.#flatCache ??= this.#toFlat();
+    return this.#flatCache;
+  }
+
+  #toFlat({ internal }: ObjectDiff<T, U> = this): [JsonPath, Diff][] {
+    const keys = Object.keys(internal).sort((a, b) => a.localeCompare(b)) as (keyof typeof internal)[];
+
+    const flat: [JsonPath, Diff][] = []
+
+    for (const key of keys) {
+      const obj = internal[key]
+
+      if (obj instanceof ObjectDiff || obj instanceof ArrayDiff) {
+        for (const [_path, _lasDiff] of obj.flat) {
+          flat.push([JsonPath.from([key, ..._path]), _lasDiff]);
+        }
+      } else {
+        flat.push([JsonPath.from([key]), obj]);
+      }
+    }
+
+    return flat;
+  }
+
 }
+
+
 export class ArrayDiff extends Diff<any, any> implements IArrayDiff {
   get [Symbol.toStringTag]() { return 'ArrayDiff'; }
 
   readonly nested = true
 
-  protected _cachedDiffValue: INestedDiff<any, any>['diff'] | null = null;
-  get diff() {
-    console.log('diff', this._cachedDiffValue);
-    if (this._cachedDiffValue === null) this.calcDeps()
-    return this._cachedDiffValue!;
+  #cachedDiffValue: INestedDiff<any, any>['diff'] | null = null;
+  get diff(): boolean {
+    if (this.#cachedDiffValue === null) this._calcDeps()
+    return this.#cachedDiffValue!;
   };
 
-  protected _cachedDiff: IArrayDiff['internal'] | null = null;
+  #cachedDiff: IArrayDiff['internal'] | null = null;
   get internal() {
-    if (this._cachedDiff === null) this.calcDeps()
-
-    return this._cachedDiff!;
+    if (this.#cachedDiff === null) this._calcDeps()
+    return this.#cachedDiff!;
   }
 
-  protected calcDeps() {
+  protected _calcDeps() {
     const diffArray: Diff[] = []
     for (let i = 0; i < Math.max(this.obj1.length, this.obj2.length); i++) {
-      const df = diff(
-        this.obj1.length > i ? this.obj1[i] : null,
-        this.obj2.length > i ? this.obj2[i] : null,
+      const df = compare(
+        this.obj1.length > i ? this.obj1[i] : undefined,
+        this.obj2.length > i ? this.obj2[i] : undefined,
       );
       diffArray.push(df);
-      this._cachedDiffValue ||= !!df.diff
+      this.#cachedDiffValue ||= !!df.diff
     }
-    this._cachedDiff = diffArray;
-    if (this._cachedDiffValue === null) this._cachedDiffValue = false
+    this.#cachedDiff = diffArray;
+    if (this.#cachedDiffValue === null) this.#cachedDiffValue = false
+  }
+
+  #flatCache: [JsonPath, Diff][] | null = null
+  get flat() {
+    this.#flatCache ??= this.#toFlat();
+    return this.#flatCache;
+  }
+
+  #toFlat({ internal }: ArrayDiff = this): [JsonPath, Diff][] {
+    let flat: [JsonPath, Diff][] = [];
+
+    for (let i = 0; i < internal.length; i++) {
+      const obj = internal[i];
+
+      if (obj instanceof ObjectDiff || obj instanceof ArrayDiff) {
+        for (const [_path, _lasDiff] of obj.flat) {
+          flat.push([JsonPath.from([i, ..._path]), _lasDiff]);
+        }
+      } else {
+        flat.push([JsonPath.from([i]), obj]);
+      }
+    }
+
+    return flat;
   }
 }
 
 
-function diff<T,U>(obj1: T, obj2: U): Diff<T,U> {
+export function compare<T extends unknown[], U extends unknown[]>(obj1: T, obj2: U): ArrayDiff
+export function compare<T extends string, U extends string>(obj1: T, obj2: U): StringDiff
+export function compare<T extends Record<string | number, unknown>, U extends Record<string | number, unknown>>(obj1: T, obj2: U): ObjectDiff<T, U>
+export function compare<T>(obj1: T, obj2: T): NoDiff<T> | ValueDiff<T, T>
+export function compare<T extends any, U extends any>(obj1: T, obj2: U): Diff<T, U>
+// export interface diff {
+//   <T>(obj1: T, obj2: T): NoDiff<T>
+//   <T extends unknown[], U extends unknown[]>(obj1: T, obj2: U): ArrayDiff
+//   <T extends string, U extends string>(obj1: T, obj2: U): StringDiff
+//   <T extends Record<string | number, unknown>, U extends Record<string | number, unknown>>(obj1: T, obj2: U): ObjectDiff<T, U>
+//   <T extends unknown, U extends unknown>(obj1: T, obj2: U): ValueDiff<T, U>
+// }
+
+export function compare<T, U>(obj1: T, obj2: U): Diff<T, U> {
   let df: Diff;
 
   if (Object.is(obj1, obj2)) {
     df = new NoDiff<T>(obj1, (obj2 as unknown) as T)
-  } else if (typeof obj1 !== typeof obj2) {
-    df = new ValueDiff(obj1, obj2)
-  } else {
-    if (Array.isArray(obj1) && Array.isArray(obj2)) {
-      df = new ArrayDiff(obj1, obj2);
-    } else if (Array.isArray(obj1) || Array.isArray(obj2) || obj1 === null || obj2 === null) {
-      df = new ValueDiff(obj1, obj2);
-    } else if (typeof obj1 === 'object') {
-      df = new ObjectDiff(obj1, obj2!);
-    } else if (typeof obj1 === 'string' && typeof obj2 === 'string') {
-      df = new StringDiff(obj1, obj2);
-    } else {
-      df = new ValueDiff(obj1, obj2);
+  } else if (typeof obj1 === typeof obj2) {
+    const type = typeof obj1
+
+    if (type === 'string') {
+      df = new StringDiff(obj1 as string, obj2 as string);
+    } else if (type === 'object' && obj1 !== null && obj2 !== null) {
+      if (Array.isArray(obj1) && Array.isArray(obj2)) {
+        df = new ArrayDiff(obj1, obj2);
+      } else if (!Array.isArray(obj1) && !Array.isArray(obj2)) {
+        df = new ObjectDiff(obj1 as any, obj2 as any);
+      }
     }
   }
+
+  df ??= new ValueDiff(obj1, obj2);
 
   return df;
 }
